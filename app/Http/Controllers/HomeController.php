8341d5ba6f3a0481c20f6ba3d53b6013f8a43161
corneliusyan <onel.raja@gmail.com>
2019-01-16 13:25:32 +0700

    Change home only shows opened projects

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,8 +9,6 @@ class HomeController extends Controller
 {
     /**
      * Create a new controller instance.
-     *
-     * @return void
      */
     public function __construct()
     {
@@ -25,16 +23,16 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         //filter projects
-        $mobile_projects = Project::where('category','Mobile')->orderBy('deadline','asc')->get();
-        $desktop_projects = Project::where('category','Desktop')->orderBy('deadline','asc')->get();
-        $web_projects = Project::where('category','Website')->orderBy('deadline','asc')->get();
-        $other_projects = Project::where('category','Other')->orderBy('deadline','asc')->get();
+        $mobile_projects = Project::where('category', 'Mobile')->where('status', 'Open')->orderBy('deadline', 'asc')->get();
+        $desktop_projects = Project::where('category', 'Desktop')->where('status', 'Open')->orderBy('deadline', 'asc')->get();
+        $web_projects = Project::where('category', 'Website')->where('status', 'Open')->orderBy('deadline', 'asc')->get();
+        $other_projects = Project::where('category', 'Other')->where('status', 'Open')->orderBy('deadline', 'asc')->get();
 
         return view('home', [
             'mobile_projects' => $mobile_projects,
             'desktop_projects' => $desktop_projects,
             'web_projects' => $web_projects,
-            'other_projects' => $other_projects
+            'other_projects' => $other_projects,
         ]);
     }
 }
